283e2bfd8c0dc4579379683b30934d1aa32951fe
ritikkannaujiya2001 <ritikkannaujiya2001@gmail.com>
2024-08-21 18:49:00 +0530

    github api integrated

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -3,25 +3,28 @@ import { IoLocationOutline } from "react-icons/io5";
 import { TfiThought } from "react-icons/tfi";
 import { FaXTwitter } from "react-icons/fa6";
 import { RiGitRepositoryFill, RiUserFollowFill, RiUserFollowLine } from "react-icons/ri";
+import { formatMemberSince } from "../utils/functions";
 
-const ProfileInfo = () => {
-    const userProfile = {
-		avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
-		bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-		email: "johndoe@gmail.com",
-		followers: 100,
-		following: 200,
-		html_url: "https://github.com/R2457",
-		location: "Somewhere, Earth",
-		name: "John Doe",
-		public_gists: 100,
-		public_repos: 100,
-		twitter_username: "johndoe",
-		login: "johndoe",
-	};
+const ProfileInfo = ({userProfile}) => {
+    // const userProfile = {
+	// 	avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
+	// 	bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
+	// 	email: "johndoe@gmail.com",
+	// 	followers: 100,
+	// 	following: 200,
+	// 	html_url: "https://github.com/R2457",
+	// 	location: "Somewhere, Earth",
+	// 	name: "John Doe",
+	// 	public_gists: 100,
+	// 	public_repos: 100,
+	// 	twitter_username: "johndoe",
+	// 	login: "johndoe",
+	// };
+
+    const memberSince = formatMemberSince(userProfile?.created_at);
     
     return (
-        <div className='lg:w-1/3 w-full flex flex-col gap-2 md:sticky md:top-10'>
+        <div className='lg:w-1/3 w-full flex flex-col gap-2 lg:sticky md:top-10'>
 			<div className='bg-glass rounded-lg p-4'>
 				<div className='flex gap-4 items-center'>
 					{/* User Avatar */}
@@ -74,7 +77,7 @@ const ProfileInfo = () => {
 				{/* Member Since Date */}
 				<div className='my-2'>
 					<p className='text-gray-600 font-bold text-sm'>Member since</p>
-					<p className=''>21 Sep, 2023</p>
+					<p className=''>{memberSince}</p>
 				</div>
 
 				{/* Email Address */}
