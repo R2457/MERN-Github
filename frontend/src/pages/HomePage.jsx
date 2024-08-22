d7d1f12251a1fd786214438e9e3ed27795927d42
ritikkannaujiya2001 <ritikkannaujiya2001@gmail.com>
2024-08-22 16:14:41 +0530

    api routes setup

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -16,23 +16,28 @@ const HomePage = () => {
 	const getUserProfileAndRepos = useCallback(async (username = "R2457") => {
 		setLoading(true);
 		try {
-		  const userRes = await fetch(`https://api.github.com/users/${username}`, {
-			headers: {
-			  authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
-			},
-		  });
+			const res = await fetch(`http://localhost:5000/api/users/profile/${username}`);
+			const {repos, userProfile} = await res.json();
+
+		//   const userRes = await fetch(`https://api.github.com/users/${username}`, {
+		// 	headers: {
+		// 	  authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+		// 	},
+		//   });
 	  
-		  const userProfile = await userRes.json();
-		  setUserProfile(userProfile);
+		//   const userProfile = await userRes.json();
+		  
 	  
-		  const repoRes = await fetch(userProfile.repos_url);
-	  
-		  const repos = await repoRes.json();
+		//   const repoRes = await fetch(userProfile.repos_url);	  
+		//   const repos = await repoRes.json();
+
 		  repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
 		  setRepos(repos);
+		  setUserProfile(userProfile);
+
+		  return { userProfile, repos };
 	  
 		} catch (error) {
-		  console.error(error);
 		  toast.error(error.message);
 		} finally {
 		  setLoading(false);
